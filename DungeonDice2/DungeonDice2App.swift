295dfb281c9a247835923ddcd5efbb25be3e0a81
Christian Manzaraz <magnusmanz@icloud.com>
2025-02-03 19:08:05 -0300

    Initial Commit

---
+++ DungeonDice2/DungeonDice2App.swift
@@ -0,0 +1,17 @@
+//
+//  DungeonDice2App.swift
+//  DungeonDice2
+//
+//  Created by Christian Manzaraz on 03/02/2025.
+//
+
+import SwiftUI
+
+@main
+struct DungeonDice2App: App {
+    var body: some Scene {
+        WindowGroup {
+            ContentView()
+        }
+    }
+}
